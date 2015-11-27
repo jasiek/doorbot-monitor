69c7afd90df5c1323251f8d2cceefed08f2f66cc
Jan Szumiec <jan.szumiec@gmail.com>
2015-11-27 14:23:31 +0000

    Restructured the monitor a bit.

--- monitor.rb
+++ monitor.rb
@@ -1,23 +1,35 @@
 require 'socket'
 require 'csv'
 
+class CSVRecorder
+  def initialize(filename)
+    @filename = filename
+  end
+
+  def notify(event, id)
+    if event == 'RFID'
+      CSV.open(@filename, "a") do |csv|
+        csv << [Time.now, id]
+        csv.fsync
+      end
+    end
+  end
+end
+
 class DoorbotMonitor
   def initialize(ports)
     @ports = ports
   end
 
-  def listen(&blk)
+  def listen(*listeners)
     sockets = @ports.map { |p| UDPSocket.new(Socket::AF_INET).tap { |u| u.bind("0.0.0.0", p) } }
     loop do
       socks, _ = IO.select(sockets)
       socks.each do |sock|
         message, _ = sock.recvfrom(255)
         event, id, _ = message.split("\n")
-        if event == 'RFID'
-	  CSV.open("logfile.csv", "a") do |csv|
-            csv << [Time.now, id]
-            csv.fsync
-          end
+        listeners.each do |listener|
+          listener.notify(event, id)
         end
       end
     end
@@ -25,5 +37,5 @@ class DoorbotMonitor
 end
 
 if __FILE__ == $0
-  DoorbotMonitor.new([50000, 50002]).listen
+  DoorbotMonitor.new([50000, 50002]).listen(CSVRecorder.new("logfile.csv"))
 end
