2f427319695a88d969ff3da21aed96e0b84bb91e
Jan Szumiec <jan.szumiec@gmail.com>
2015-11-27 14:43:41 +0000

    Make snaps!

--- monitor.rb
+++ monitor.rb
@@ -1,16 +1,46 @@
 require 'socket'
 require 'csv'
+require 'fileutils'
+require 'net/http'
+require 'cgi'
+require 'pp'
 
 class CSVRecorder
   def initialize(filename)
     @filename = filename
   end
 
-  def notify(event, id)
+  def notify(port, event, id)
     if event == 'RFID'
       CSV.open(@filename, "a") do |csv|
-        csv << [Time.now, id]
+        csv << [Time.now, id, port]
         csv.fsync
+      end
+    end
+  end
+end
+
+class Snapshotter
+  CAMERA_URL = URI('https://london.hackspace.org.uk/members/camera.php?id=3')
+  
+  def initialize(directory)
+    FileUtils.mkdir_p(@directory = directory)
+    @cookie = CGI::Cookie.new('PHPSESSID', File.read('session.key'))
+  end
+
+  def notify(port, event, id)
+    if event == 'RFID' && port == 50002
+      filename = Time.now.strftime("%Y-%m-%d_%H:%M_#{id}.jpg")
+      http = Net::HTTP.new(CAMERA_URL.host, CAMERA_URL.port)
+      http.use_ssl = true
+      request = Net::HTTP::Get.new(CAMERA_URL.request_uri)
+      request['Cookie'] = @cookie.to_s
+      response = http.request(request)
+
+      if response.code == '200'
+        File.open(File.join(@directory, filename), 'w') do |f|
+          f.write(response.body)
+        end
       end
     end
   end
@@ -26,10 +56,11 @@ class DoorbotMonitor
     loop do
       socks, _ = IO.select(sockets)
       socks.each do |sock|
+        _, port, _, _ = sock.addr
         message, _ = sock.recvfrom(255)
         event, id, _ = message.split("\n")
         listeners.each do |listener|
-          listener.notify(event, id)
+          listener.notify(port, event, id)
         end
       end
     end
@@ -37,5 +68,5 @@ class DoorbotMonitor
 end
 
 if __FILE__ == $0
-  DoorbotMonitor.new([50000, 50002]).listen(CSVRecorder.new("logfile.csv"))
+  DoorbotMonitor.new([50000, 50002]).listen(CSVRecorder.new("logfile.csv"), Snapshotter.new('snaps'))
 end
